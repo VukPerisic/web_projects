c0eff690972b20f711e4b258ec118924c14b392e
VukPerisic <86321943+VukPerisic@users.noreply.github.com>
2022-05-01 23:38:48 -0700

    Create Comment.js

---
+++ hw6_react/Comment.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Comment = ({text}) => {
+    return (
+        <div>
+            <h3>
+                {text}
+            </h3>
+        </div>
+    )
+};
+
+export default Comment;
