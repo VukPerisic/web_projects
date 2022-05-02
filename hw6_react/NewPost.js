142377a61ca0b0e71259e1b9f4aeb1f6f021a578
VukPerisic <86321943+VukPerisic@users.noreply.github.com>
2022-05-01 23:39:17 -0700

    Create NewPost.js

---
+++ hw6_react/NewPost.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import {useState, useEffect} from "react";
+import Feed from "./Feed.js";
+
+
+const NewPost = () => {
+  const [id, setId] = useState();
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
+
+  const onSubmit = () => {
+    console.log({
+      id,
+      title,
+      body
+    });
+    axios
+      .post("http://localhost:3002/posts", {"id": id, "title":title, "body":body})
+      .then( (response) => console.log(response))
+      .catch( (error) => console.log(error));
+    return <Feed />;
+  };
+
+
+
+
+  return <div>
+    <div>
+      <input type="text" placeholder="ID" value={id} onChange={e => setId(e.target.value)} />
+    </div>
+    <div>
+      <input type="text" placeholder="Title" value={title} onChange={e => setTitle(e.target.value)} />
+    </div>
+    <div>
+      <input type="text" placeholder="Body" value={body} onChange={e => setBody(e.target.value)} />
+    </div>
+    <button style={{ marginTop: '4px'}} onClick={onSubmit}>
+      Submit
+    </button>
+  </div>
+}
+
+export default NewPost;
